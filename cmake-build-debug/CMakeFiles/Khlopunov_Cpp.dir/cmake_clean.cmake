33cd047f72d733b2dde5786d0378df74540cb6da
Khlff <n.hlopunov@ya.ru>
2021-09-26 21:05:21 +0500

    test

---
+++ cmake-build-debug/CMakeFiles/Khlopunov_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Khlopunov_Cpp.dir/CW-1-1/CW-1-1.cpp.o"
+  "Khlopunov_Cpp.exe"
+  "Khlopunov_Cpp.pdb"
+  "libKhlopunov_Cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Khlopunov_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
