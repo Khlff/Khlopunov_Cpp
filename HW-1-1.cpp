3b6660236ed74e9dadaa1df5712761d10766b647
Khlff <n.hlopunov@ya.ru>
2021-09-26 22:29:48 +0500

    update

--- HW-1-1.cpp
+++ HW-1-1.cpp
@@ -2,12 +2,13 @@
 #include <cmath>
 
 int main() {
-    int a, b, c;
+    double a, b, c;
     std::cin >> a >> b >> c;
 
-    double Discriminant = pow(b,2)-4*a*c;
-    double x1= (-b - sqrt(Discriminant))/(2*a);
-    double x2= (-b + sqrt(Discriminant))/(2*a);
+    double Discriminant = pow(b, 2) - 4 * a * c;
+
+    double x1 = (-b - sqrt(Discriminant)) / (2 * a);
+    double x2 = (-b + sqrt(Discriminant)) / (2 * a);
 
     std::cout << x1 << " " << x2;
 
