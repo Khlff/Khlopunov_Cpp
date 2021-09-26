8426ccc169de36e6381e5a81374613592cea2cb4
Khlff <n.hlopunov@ya.ru>
2021-09-26 22:17:45 +0500

    new branch

---
+++ HW-1-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    double N, A, B, X, Y;
+    std::cin >> N >> A >> B >> X >> Y;
+
+    if (N > B) {
+        std::cout << (N * (1 - Y / 100));
+    } else if (N > A) {
+        std::cout << N * (1 - X / 100);
+    } else {
+        std::cout << N;
+    }
+
+    return 0;
+}
