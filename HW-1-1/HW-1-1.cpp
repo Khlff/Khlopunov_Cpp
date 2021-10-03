7ca5e741de807d0e7a1ac635ec5302851420a255
khlff <n.hlopunov@ya.ru>
2021-10-03 21:14:36 +0500

    [HW-1-1] Update

--- HW-1-1/HW-1-1.cpp
+++ HW-1-1/HW-1-1.cpp
@@ -2,18 +2,18 @@
 #include <cmath>
 
 int main() {
-    double quadraticСoefficient, linearСoefficient, freeСoefficient;
-    std::cin >> quadraticСoefficient >> linearСoefficient >> freeСoefficient;
+    double quadCoeff, linСoeff, freeCoeff;
+    std::cin >> quadCoeff >> linСoeff >> freeCoeff;
 
-    if (quadraticСoefficient != 0) {
-        double Discriminant = pow(linearСoefficient, 2) - 4 * quadraticСoefficient * freeСoefficient;
+    if (quadCoeff != 0) {
+        double Discriminant = pow(linСoeff, 2) - 4 * quadCoeff * freeCoeff;
 
         if (Discriminant < 0) {
             return 1;
         }
 
-        double x1 = (-linearСoefficient - sqrt(Discriminant)) / (2 * quadraticСoefficient);
-        double x2 = (-linearСoefficient + sqrt(Discriminant)) / (2 * quadraticСoefficient);
+        double x1 = (-linСoeff - sqrt(Discriminant)) / (2 * quadCoeff);
+        double x2 = (-linСoeff + sqrt(Discriminant)) / (2 * quadCoeff);
 
         if (x1 == x2) {
             std::cout << x1;
@@ -21,10 +21,10 @@ int main() {
             std::cout << x1 << " " << x2;
         }
 
-    } else if (freeСoefficient != 0) {
-        std::cout << -freeСoefficient / linearСoefficient;
+    } else if (freeCoeff != 0) {
+        std::cout << -freeCoeff / linСoeff;
     } else {
-        std::cout << 0;
+        return 1
     }
 
     return 0;
