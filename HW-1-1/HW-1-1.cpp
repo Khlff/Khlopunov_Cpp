c6e4782e32ddb019c1a67bac8aa8c8a0d3a46381
khlff <n.hlopunov@ya.ru>
2021-10-03 21:18:39 +0500

    [HW-1-1] Update

--- HW-1-1/HW-1-1.cpp
+++ HW-1-1/HW-1-1.cpp
@@ -6,14 +6,14 @@ int main() {
     std::cin >> quadCoeff >> linСoeff >> freeCoeff;
 
     if (quadCoeff != 0) {
-        double Discriminant = pow(linСoeff, 2) - 4 * quadCoeff * freeCoeff;
+        double discriminant = pow(linСoeff, 2) - 4 * quadCoeff * freeCoeff;
 
-        if (Discriminant < 0) {
+        if (discriminant < 0) {
             return 1;
         }
 
-        double x1 = (-linСoeff - sqrt(Discriminant)) / (2 * quadCoeff);
-        double x2 = (-linСoeff + sqrt(Discriminant)) / (2 * quadCoeff);
+        double x1 = (-linСoeff - sqrt(discriminant)) / (2 * quadCoeff);
+        double x2 = (-linСoeff + sqrt(discriminant)) / (2 * quadCoeff);
 
         if (x1 == x2) {
             std::cout << x1;
@@ -21,7 +21,7 @@ int main() {
             std::cout << x1 << " " << x2;
         }
 
-    } else if (freeCoeff != 0) {
+    } else if (linСoeff != 0) {
         std::cout << -freeCoeff / linСoeff;
     } else {
         return 1
