b96609e12de0df73f22b9a0f4cf5b0494032b56e
khlff <n.hlopunov@ya.ru>
2021-10-06 21:40:44 +0500

    [HW-2-2] требуются доработки

---
+++ HW-2-2/HW-2-2.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    int fXFirst, fYFirst, fXSecond, fYSecond, fXThird, fYThird, fXFourth, fYFourth, sXFirst, sYFirst, sXSecond, sYSecond, sXThird, sYThird, sXFourth, sYFourth;
+    std::cin >> fXFirst >> fYFirst >> fXSecond >> fYSecond >> fXThird >> fYThird >> fXFourth >> fYFourth >> sXFirst
+             >> sYFirst >> sXSecond >> sYSecond >> sXThird >> sYThird >> sXFourth >> sYFourth;
+
+    int leftEdge = std::max(fXFirst, sXFirst);
+    int bottomEdge = std::max(fYFirst, sYFirst);
+    int topEdge = std::min(fYThird, sYThird);
+    int rightEdge = std::min(fXFourth, sXFourth);
+
+    int width = rightEdge - leftEdge;
+    int height = topEdge - bottomEdge;
+
+    if (width < 0 or height < 0) {
+        std::cout << "NO";
+        return 1;
+    }
+    std::cout << "YES ";
+    std::cout << width * height;
+    return 0;
+
+}
