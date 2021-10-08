1cd44d164a723cc51ca3b8c56edb057963cd6f28
Khlff <n.hlopunov@ya.ru>
2021-10-08 19:36:41 +0500

    [CW-3-1] Loaded

---
+++ CW-3-1/CW-3-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    using namespace std;
+    string inputString;
+    cin >> inputString;
+
+    for (int i = 0; i <= (inputString.length() / 2); i++) {
+        if (inputString[i] != inputString[inputString.length() - i - 1]) {
+            cout << "False";
+            return 1;
+        }
+    }
+    cout << "True";
+    return 0;
+}
