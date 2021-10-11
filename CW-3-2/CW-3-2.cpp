bf1440db09b642a186b3fdcad316d127de681c72
Khlff <n.hlopunov@ya.ru>
2021-10-11 21:34:57 +0500

    [CW-3-2] updated

--- CW-3-2/CW-3-2.cpp
+++ CW-3-2/CW-3-2.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <algorithm>
 
+using namespace std;
+
 int main() {
-    using namespace std;
     string inputString;
     cin >> inputString;
 
@@ -12,15 +13,17 @@ int main() {
             cout << inputString[i + 1] << inputString[i];
             break;
         }
-        if (inputString[inputString.length() - 1] == inputString[inputString.length() - 2]) {
-            cout << inputString[inputString.length() - 1] << inputString[inputString.length() - 2];
-            break;
-        }
         if (inputString[i] == inputString[i + 2]) {
+            if (inputString[inputString.length() - 1] == inputString[inputString.length() - 2]) {
+                cout << inputString[inputString.length() - 1] << inputString[inputString.length() - 2];
+                break;
+            }
             cout << inputString[i] << inputString[i + 1] << inputString[i + 2];
             break;
         }
-
+    }
+    if (inputString[inputString.length() - 1] == inputString[inputString.length() - 2]) {
+        cout << inputString[inputString.length() - 1] << inputString[inputString.length() - 2];
     }
     return 0;
 }
