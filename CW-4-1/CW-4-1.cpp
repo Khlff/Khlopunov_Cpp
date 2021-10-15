ae9ec38a7a38adcb23f241441efea335ae408c43
khlff <n.hlopunov@ya.ru>
2021-10-15 11:42:20 +0500

    [CW-4-1] update

--- CW-4-1/CW-4-1.cpp
+++ CW-4-1/CW-4-1.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 
 vector<string> palindromeFilter(int minLength, vector<string> words) {
-    vector<string> outputVector= {};
+    vector<string> outputVector = {};
     for (int i = 0; i < words.size(); i++) {
         bool isPalindrome = true;
-        if (words[i].length() <= minLength){
+        if (words[i].length() <= minLength) {
             continue;
-        }
-        else {
+        } else {
             string inputString = words[i];
             for (int j = 0; j <= (inputString.length() / 2); j++) {
                 if (inputString[j] != inputString[inputString.length() - j - 1]) {
@@ -18,7 +17,7 @@ vector<string> palindromeFilter(int minLength, vector<string> words) {
                     break;
                 }
             }
-            if (isPalindrome){
+            if (isPalindrome) {
                 outputVector.push_back(words[i]);
             }
         }
@@ -30,8 +29,9 @@ int main() {
     int min;
     cin >> min;
     vector<string> inputVector = {"weew", "aba", "bro"};
-    for (int i = 0; i < palindromeFilter(min, inputVector).size(); i++) {
-        cout << palindromeFilter(min, inputVector)[i] << " ";
+    vector<string> answerVector = palindromeFilter(min, inputVector);
+    for (int i = 0; i < answerVector.size(); i++) {
+        cout << answerVector[i] << " ";
     }
     return 0;
 }
