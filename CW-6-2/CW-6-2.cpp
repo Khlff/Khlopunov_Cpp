102026c1abc28bd057cd6d97fc4a35e312b7862b
khlff <n.hlopunov@ya.ru>
2021-10-29 11:41:14 +0500

    [CW-6-2] upd

--- CW-6-2/CW-6-2.cpp
+++ CW-6-2/CW-6-2.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector<string> wasteErasing(string rawString, int maxLength) {
+string wasteErasing(string rawString, int maxLength) {
     vector<string> wordsList;
     string tempWord = "";
     for (int i = 0; i < rawString.length(); i++) {
@@ -23,17 +23,20 @@ vector<string> wasteErasing(string rawString, int maxLength) {
     if (tempWord.length() > maxLength) {
         wordsList.push_back(tempWord);
     }
-    return wordsList;
+    string outputString = "";
+    for (int i = 0; i < wordsList.size(); i++) {
+        outputString += wordsList[i] + ' ';
+    }
+
+    return outputString;
 }
 
 int main() {
     int maxLength;
     cin >> maxLength;
     string inputString = "Example for you abracadabra";
-    vector<string> outputWords = wasteErasing(inputString, maxLength);
-    for (int i = 0; i < outputWords.size(); i++) {
-        cout << outputWords[i] << ' ';
-    }
+    string outputWords = wasteErasing(inputString, maxLength);
+    cout << outputWords;
     return 0;
 }
 
