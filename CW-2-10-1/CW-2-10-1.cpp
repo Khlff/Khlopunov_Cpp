464add43657f84663b92e9d4af1f0cc96ce6284a
Khlff <1Ghlmncxw3456>
2022-06-02 15:49:27 +0500

    [CW-2-10-1]

---
+++ CW-2-10-1/CW-2-10-1.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "LinkedListStack.h"
+
+int main() {
+    std::unique_ptr<LinkedListStack> myStack = std::make_unique<LinkedListStack>();
+    myStack->push();
+    myStack->push();
+    myStack->push();
+    myStack->push();
+    int a = myStack->getTop();
+    int b = myStack->pop();
+    int c = myStack->pop();
+    std::cout << a << " " << b << " " << c;
+
+    return 0;
+}
